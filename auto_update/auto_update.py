2720c0fa2a83a54316a347414c7f66e07c9afe6c
drive-desaster <78107638+drive-desaster@users.noreply.github.com>
2021-01-29 00:10:16 +0100

    commenting
    added comments

--- auto_update/auto_update.py
+++ auto_update/auto_update.py
@@ -15,31 +15,39 @@ if not os.path.exists('.version') :
 #'config' file
 file = open("pks",'r')
 
-for package in file.readlines() :
-    package = package.replace('\n', '')
+for package in file.readlines() :       #packete zeile für zeile lesen
+    package = package.replace('\n', '') 
     
     #Überprüfen, ob das packet aktuell ist
-    r = requests.get('https://aur.archlinux.org/rpc/?v=5&type=search&arg=' + package)
+    r = requests.get('https://aur.archlinux.org/rpc/?v=5&type=search&arg=' + package) #link: https://wiki.archlinux.org/index.php/Aurweb_RPC_interface
     res = r.json()
     version_new = res['results'][0]['Version'] 
-    if not os.path.isfile(".version/" + package + '.v') :
+    
+    
+    #wenn noch nie behandelt, datei mit version für packet erstellen und neue versin hinein schreiben
+    if not os.path.isfile(".version/" + package + '.v') : 
         v = open(".version/" + package + '.v', 'w')
         v.write(version_new)
-        version_old = 0
+        version_old = 0 # keine Versin vorhanden resultiert in versin 0
         v.close()
-    else :
+    
+    
+    else :      #wenn versions datei vorhanden, vorhandene version lesen und neu version in datei schreiben
         v = open(".version/" + package + '.v', 'r')
-        version_old = v.read()
+        version_old = v.read()                          
         v.close()
         v = open(".version/" + package + '.v', 'w')
         v.write(version_new)
         v.close()
+        
+        # wenn versionen unterschiedlich
     if version_new != version_old :
         print(f"package: {package}")
-        os.system(f"git clone https://aur.archlinux.org/{package}.git")
-        os.chdir(package)
+        os.system(f"git clone https://aur.archlinux.org/{package}.git") #packet 'herrunterladen'
+        os.chdir(package) 
         print(f"Building {package}")
-        os.system("makepkg")
+        
+        os.system("makepkg")    #   packet bauen
         
         print("copying filtes to pkg dir")
         for pkg in os.listdir() :
@@ -50,5 +58,5 @@ for package in file.readlines() :
         os.chdir("..")
         shutil.rmtree(package)   #dateinen löschen
     else :
-        print(f"{package} is already up to date (install from ./pkg/)")
+        print(f"{package} is already up to date (install from ./pkg/)") #wenn versionen gleich sind, dies ausgeben
 file.close()
